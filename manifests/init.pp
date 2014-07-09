88d556d13fd773dd61833fc3d39f1574b01c4a8f
Tim Freund <tim@freunds.net>
2014-07-09 01:13:32 -0400

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,74 @@
+# == Class: kallitheaci
+#
+# Configure CI services for the Kallithea project.
+#
+# === Parameters
+#
+# None
+
+# === Variables
+#
+# None
+#
+# === Examples
+#
+#  include kallitheaci
+#
+# === Authors
+#
+# Tim Freund <tim@freunds.net>
+#
+# === Copyright
+#
+# Copyright 2014 Tim Freund
+#
+class kallitheaci {
+  include jenkins
+  
+  jenkins::plugin {
+    "docker-plugin": ;
+  }
+
+  jenkins::plugin {
+    "git": ;
+  }
+
+  jenkins::plugin {
+    "mercurial": ;
+  }
+
+
+  package {'apt-transport-https':
+    # required to use docker's apt repo
+    ensure => installed,
+    before => Package['lxc-docker'],
+  }
+
+  apt::source {'docker':
+    location          => 'https://get.docker.io/ubuntu',
+    release => '',
+    repos => 'docker main',
+    key               => '36A1D7869245C8950F966E92D8576A8BA88D21E9',
+    key_server        => 'keyserver.ubuntu.com',
+    include_src => false,
+    before => Package['lxc-docker'],
+  }
+
+  package {'lxc-docker':
+    # This is pulled from Docker's repo, so we get a version that
+    # is compatible with the Jenkins docker plugin.
+    ensure => installed,
+  }
+
+  file {'/etc/default/docker':
+    source => "puppet:///modules/kallitheaci/default-docker",
+    notify => Service["docker"],
+    mode => 600,
+    owner => root,
+    group => root,
+  }
+
+  service {'docker':
+    ensure => running,
+  }
+}
